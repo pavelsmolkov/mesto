4b72c51a4a4be90591e1827857d8e48ff78abe6e
Pavel Smolkov <pavelsmolkov@yandex.ru>
2021-01-19 22:24:34 +0300

    feat: added cards render

---
+++ scriptCards.js
@@ -0,0 +1,44 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const itemTemplate = document.querySelector('.item_template').content;
+const cards = document.querySelector('.cards');
+const cardImage = itemTemplate.querySelector('.card__image');
+
+function render() {
+	initialCards.forEach(renderCard);
+}
+
+function renderCard(initialCards) {
+  const htmlElement = itemTemplate.cloneNode(true);
+
+  htmlElement.querySelector('.card__header').textContent = initialCards.name;
+  htmlElement.querySelector('.card__image').setAttribute('src', initialCards.link);  
+  cards.appendChild(htmlElement);
+}
+
+render();
