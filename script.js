ef1efea3e126bde0723009da1af5ebbeaf0df7f5
Pavel Smolkov <pavelsmolkov@yandex.ru>
2021-01-21 00:33:55 +0300

    feat: like function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let popupEdit = document.querySelector('.popup_edit');
+let popupEdit = document.querySelector('.popup_edit'); 
 let popupAdd = document.querySelector('.popup_add');
 let formElement = document.querySelector('.popup__container');
 let editButton = document.querySelector('.profile__edit-button');
@@ -12,29 +12,31 @@ let profileSubtitle = document.querySelector('.profile__subtitle');
 let nameInput = document.querySelector('.popup__field_input_name');
 let jobInput = document.querySelector('.popup__field_input_job');
 
+
+// открытие попапа профиля
 function openPopup() {
   popupEdit.classList.toggle('popup_opened');
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
 }
 
+//закрытие попапа профиля
 function closePopup(item) {
   item.classList.remove('popup_opened');
 }
 
+//открытие попапа добавления места
 function openAddPopup() {
   resetPopup();
   popupAdd.classList.toggle('popup_opened');
-  // nameInput.value = profileTitle.textContent;
-  // jobInput.value = profileSubtitle.textContent;
 }
 
+//закрытие попапа добавления места
 function closeAddPopup() {
   popupAdd.classList.remove('popup_opened');
 }
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+// Обработчик отправки формы профиля
 function handleFormSubmit (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 
@@ -43,16 +45,22 @@ function handleFormSubmit (evt) {
   closePopup(popupEdit);
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+// слушатель кнопки редактирования профиля
 editButton.addEventListener('click', openPopup);
+
+//слушатель кнопки закрытия попапа редактирования профиля
 closeButtonEdit.addEventListener('click', () => closePopup(popupEdit));
+
+//слушатель кнопки Сохранить редактирования профиля
 formElement.addEventListener('submit', handleFormSubmit);
 
+//слушатель кнопки Плюс добавления новой карточки
 addButton.addEventListener('click', openAddPopup);
+
+//слушатель кнопки закрытия попапа новой карточки
 closeButtonAdd.addEventListener('click', () => closePopup(popupAdd));
 
-
+//массив карточек для начального рендера
 const initialCards = [
   {
     name: 'Архыз',
@@ -74,11 +82,11 @@ const initialCards = [
     name: 'Холмогорский район',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   }
-  // ,
-  // {
-  //   name: 'Байкал',
-  //   link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  // }
+  ,
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
 ];
 
 const itemTemplate = document.querySelector('.item_template').content;
@@ -86,15 +94,19 @@ const cards = document.querySelector('.cards');
 const cardImage = itemTemplate.querySelector('.card__image');
 
 
-
 function render() {
-	initialCards.forEach(renderCard);
+  initialCards.forEach(renderCard);
+}
+
+function renderAdded() {
+  const allCards = document.querySelectorAll('.cards');
 }
 
 function renderCard(initialCards) {
   const htmlElement = itemTemplate.cloneNode(true);
   htmlElement.querySelector('.card__header').textContent = initialCards.name;
-  htmlElement.querySelector('.card__image').setAttribute('src', initialCards.link);  
+  htmlElement.querySelector('.card__image').setAttribute('src', initialCards.link); 
+  handleLike(htmlElement);
   cards.appendChild(htmlElement);
 }
 
@@ -104,18 +116,29 @@ const createButton = document.querySelector('.popup__create-button');
 const cardInputName = document.querySelector('.popup__field_input_place');
 const cardInputImage = document.querySelector('.popup__field_input_url');
 
+createButton.addEventListener('click', handleAddCard);
+
 function handleAddCard (evt) {
   evt.preventDefault();
   const htmlElement = itemTemplate.cloneNode(true);
   htmlElement.querySelector('.card__header').textContent = cardInputName.value;
   htmlElement.querySelector('.card__image').setAttribute('src', cardInputImage.value);  
+  handleLike(htmlElement);
   cards.prepend(htmlElement);
   closePopup(popupAdd);
-} 
+  renderAdded()
+
+}
+
+function handleLike(htmlElement) {
+  const cardLike = htmlElement.querySelector('.card__like');
+  cardLike.addEventListener('click', function (evt) {
+    evt.target.classList.toggle('card__like_active');
+  });
+}
+
 
 function resetPopup() {
   cardInputName.value = "";
   cardInputImage.value = "";
 }
-
-createButton.addEventListener('click', handleAddCard);
