643d4fe6bc6fb75f830748b0990aec69b816ac4f
Pavel Smolkov <pavelsmolkov@yandex.ru>
2021-05-13 01:50:35 +0300

    UserInfo WIP

---
+++ scripts/UserInfo.js
@@ -0,0 +1,16 @@
+export default class UserInfo {
+    constructor(nameUserSelector, jobUserSelector) {
+        this._nameUserSelector = nameUserSelector;
+        this._jobUserSelector = jobUserSelector;
+    }
+
+    getUserInfo() {
+
+        // return userData
+    }
+
+    setUserInfo() {
+
+    }
+
+}
