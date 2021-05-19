41a7218befc6aee94a5ef6d3ae42a67d683277ea
Pavel Smolkov <pavelsmolkov@yandex.ru>
2021-05-19 20:28:44 +0300

    project strucure refactoring-3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
-import './index.css';
-import Card from "../components/Card.js";
-import FormValidator from "../components/FormValidator.js";
+import "./index.css";
+
 import Section from "../components/Section.js";
 import Popup from "../components/Popup.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
+import Card from "../components/Card.js";
+import FormValidator from "../components/FormValidator.js";
 
 export { ESC, addCardForm, editProfileForm };
 
